2c273ef18f9f07f6d31aa51899a9f70cdc2fa3f5
Estelle Choi <estele.choi@gmail.com>
2022-05-12 13:40:04 +0900

    add: largest subset where bitwise AND gets positive - 다시 정리

--- geeksforgeeks/largestSubset.ts
+++ geeksforgeeks/largestSubset.ts
@@ -1,7 +1,26 @@
 // https://www.geeksforgeeks.org/find-the-size-of-largest-subset-with-positive-bitwise-and/
 
-type Bit = 0 | 1;
+function largestSubsetSize(arr: number[]): number {
+    let counts = new Array(32).fill(0) // the number of 1s at each bit position of each number from array
 
-function largestSubset(a: number[], N: number) {
+    for (let i = 0; i < arr.length; i++) {
+        let x = 31 // from the rightmost bit position
 
+        while (arr[i] > 0) {
+            if ((arr[i] & 1) === 1) { // check if the rightmost bit is set bit or not
+                counts[x] += 1
+            }
+
+            arr[i] >>= 1 // shift 1 bit to the right
+            x -= 1
+        }
+    }
+
+    let max = 0
+
+    for (let i = 0; i < 32; i++) {
+        max = Math.max(max, counts[i])
+    }
+
+    return max
 }
